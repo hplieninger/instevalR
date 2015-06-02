89e48bdb883ca477489974eafdc900729a16f6ba
hplieninger <plieninger@uni-mannheim.de>
2015-06-02 14:24:03 +0200

    read_eval: Documentation

--- R/read_eval.R
+++ R/read_eval.R
@@ -1,14 +1,33 @@
-library("plyr")
-read_eval <- function(directory, id, return.raw = FALSE) {
+#' Read insteval data from directory
+#'
+#' Reads all *.csv-files from a given directory and returns all content in a single list.
+#'
+#' @param directory Character string of path name, where the *.csv-files are stored.
+#' @param id Optional numeric vector identifying a subset of files, which should be read.
+#' @return Returns a list of data frames, one for each *.csv-file.
+#' @export
+#' @importFrom reshape2 dcast
+#' @
+#' @examples
+#' \dontrun{
+#' dat.1 <- read_eval("./data/")            # read all files
+#' dat.1 <- read_eval("./data/", id = 1:5)  # read first 5 files (sorted by file name)
+#' }
+# library("reshape2")
+read_eval <- function(directory, id) {
 
     files <- list.files(directory, full.names = F)
-    files <- list.files("C:/Users/plieninger/Documents/Teaching/Evaluation/Daten/", full.names = F)
-    files <- substr(files, start = 23, stop = 100)
-    order.1 <- order(nchar(files))
+    if (all(substr(files, start = 1, stop = 22) == "InstEvaL-Rohdaten-vlg_")) {
+        files <- substr(files, start = 1 + unlist(gregexpr("_", files)), stop = 100)
+        order.1 <- order(nchar(files))
 
-    file.names <- gregexpr("-", files[order.1])
-    file.names <- substr(files[order.1], start = 1, stop = as.numeric(file.names)-1)
-    files <- list.files(directory, full.names = T)[order.1]
+        file.names <- gregexpr("-", files[order.1])
+        file.names <- substr(files[order.1], start = 1, stop = unlist(file.names)-1)
+        files <- list.files(directory, full.names = T)[order.1]
+    } else {
+        file.names <- files
+    }
+
 
     if (missing(id)) id <- 1:length(files)
 
