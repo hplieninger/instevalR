dc7974145ebc6837dfaf3b2635e4a230c81e5577
hplieninger <plieninger@uni-mannheim.de>
2015-06-05 16:30:51 +0200

    comb_eval: changed input to function from list to data frame

--- R/comb_eval.R
+++ R/comb_eval.R
@@ -13,13 +13,15 @@
 #'  \item{labels}{For each scale, its label}
 #' }
 #' @export
-#' @importFrom plyr rbind.fill
+#' @importFrom plyr ddply
+#' @importFrom reshape2 dcast
 #' @examples
 #' \dontrun{
 #' dat.1 <- read_eval("./data/")            # read all files
 #' res.1 <- comb_eval(dat.1)                # combine results for plotting
 #' }
 comb_eval <- function(dat, id) {
+    if (is.data.frame(dat) != TRUE) stop("Object 'dat' must be a data frame")
     # library("plyr")
     if (missing(id)) id <- 1:length(dat)
     dat <- dat[id]
@@ -27,44 +29,48 @@ comb_eval <- function(dat, id) {
     # labels <- read.csv("~/Teaching/Evaluation/instevalR/data-raw/labels.csv", sep = ";", header = T)
     labels.2 <- labels[, c("nummer", "scale")]
     scale <- as.character(labels$thema)
-#     scale[scale == "Gesamtbewertung"] <- c("Ges_Didaktische Fähigkeiten",
-#                                            "Ges_Note Dozent",
-#                                            "Ges_Note LV",
-#                                            "Ges_Vergleich mit anderen LVn")
-#     scale[scale == "Rahmenbedingungen"] <- paste0("Rahmenbedingungen", 101:111)
-#     scale[scale == "Referate"] <- paste0("Referate", 1:6)
-#     scale <- gsub(" ", ".", scale)
-#
-#     labels.2 <- data.frame(nummer = labels$nummer
-#                            , scale = factor(scale)
-#                            , thema = labels$thema
-#                            , text = labels$text
-#     )
-    file.names <- names(dat)
+    # file.names <- names(dat)
+
+    dat <- merge(dat, labels.2, by.x = "number", by.y = "nummer")[, c(2, 3, 4, 5)]
 
     #####################
 
-    dat.2 <- lapply(dat.1, function(x) data.frame(nummer = colnames(x), t(x)))
-    dat.3 <- lapply(dat.2, function(x) merge(x = x, y = labels.2, by = "nummer"))
-    dat.4 <- lapply(dat.3, function(x) aggregate(x[, -c(1, ncol(x))], list(x[, "scale"]), mean, na.rm = T))
-    dat.5 <- lapply(dat.4, function(x) {
-        y <- data.frame(t(x[, -1]))
-        colnames(y) <- x[, 1]
-        return (y)
-    })
+    dat.m <- ddply(dat, .variables = c("file", "scale"), .fun = colwise(mean, na.rm = T))
+    dat.m <- dcast(dat.m, formula = file ~ scale, value.var = "resp")[, -1]
+    dat.sd <- ddply(dat, .variables = c("file", "scale"), .fun = colwise(sd, na.rm = T))
+    dat.sd <- dcast(dat.sd, formula = file ~ scale, value.var = "resp")[, -1]
+    dat.n <- ddply(dat, .variables = c("file", "scale"), .fun = nrow)
+    dat.n <- dcast(dat.n, formula = file ~ scale, value.var = "V1")[, -1]
+    dat.se <- dat.sd / sqrt(dat.n)
 
-    dat.5 <- lapply(dat.5, function(x) {
-        cbind(x, Gesamt = rowMeans(x[, grep("Ges_", colnames(dat.5[[1]]))], na.rm = T))
-    })
+    rownames(dat.m) <- rownames(dat.sd) <- rownames(dat.n) <- rownames(dat.se) <- levels(dat.1$file)
+    # colnames(dat.m) <- colnames(dat.sd) <- colnames(dat.n) <- colnames(dat.se) <- labels.2$scale[order(labels.2$nummer)]
 
-    dat.m <- rbind.fill(lapply(dat.5, function(x) data.frame(t(colMeans(x, na.rm = T)))))
-    dat.sd <- rbind.fill(lapply(dat.5, function(x) {
-        data.frame(t(apply(x, 2, sd, na.rm = T)))
-    }))
-    dat.se <- rbind.fill(lapply(dat.5, function(x) {
-        data.frame(t(apply(x, 2, sd, na.rm = T)))/sqrt(nrow(x))
-    }))
-    row.names(dat.m) <- row.names(dat.sd) <- row.names(dat.se) <- file.names
+    labels.2$scale[order(labels.2$nummer)]
+
+    #####################
+
+#     dat.2 <- lapply(dat.1, function(x) data.frame(nummer = colnames(x), t(x)))
+#     dat.3 <- lapply(dat.2, function(x) merge(x = x, y = labels.2, by = "nummer"))
+#     dat.4 <- lapply(dat.3, function(x) aggregate(x[, -c(1, ncol(x))], list(x[, "scale"]), mean, na.rm = T))
+#     dat.5 <- lapply(dat.4, function(x) {
+#         y <- data.frame(t(x[, -1]))
+#         colnames(y) <- x[, 1]
+#         return (y)
+#     })
+#
+#     dat.5 <- lapply(dat.5, function(x) {
+#         cbind(x, Gesamt = rowMeans(x[, grep("Ges_", colnames(dat.5[[1]]))], na.rm = T))
+#     })
+#
+#     dat.m <- rbind.fill(lapply(dat.5, function(x) data.frame(t(colMeans(x, na.rm = T)))))
+#     dat.sd <- rbind.fill(lapply(dat.5, function(x) {
+#         data.frame(t(apply(x, 2, sd, na.rm = T)))
+#     }))
+#     dat.se <- rbind.fill(lapply(dat.5, function(x) {
+#         data.frame(t(apply(x, 2, sd, na.rm = T)))/sqrt(nrow(x))
+#     }))
+#     row.names(dat.m) <- row.names(dat.sd) <- row.names(dat.se) <- file.names
 
     x1 <- c(grep("Ges", colnames(dat.m)),
             grep("Rahm", colnames(dat.m)),
@@ -78,10 +84,11 @@ comb_eval <- function(dat, id) {
             grep("Sonst_", colnames(dat.m)))
     dat.m <- dat.m[, x3]
     dat.sd <- dat.sd[, x3]
+    dat.n <- dat.n[, x3]
     dat.se <- dat.se[, x3]
 
-    N <- unlist(lapply(dat.4, ncol)) - 1
+    # N <- unlist(lapply(dat.4, ncol)) - 1
 
-    return(list(mean = dat.m, sd = dat.sd, se = dat.se, N = N,
+    return(list(mean = dat.m, sd = dat.sd, se = dat.se, N = dat.n,
                 labels = labels$labels[match(colnames(dat.m), labels.2$scale)]))
 }
